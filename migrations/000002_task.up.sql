7931f384cc8654a494751e27f9602a571a2603b8
andy.lin <andy_lin@tomofun.com>
2023-12-15 11:27:40 +0800

    change keyword

---
+++ migrations/000002_task.up.sql
@@ -0,0 +1,14 @@
+create table tasks
+(
+    id          text not null
+        primary key,
+    prompt         text,
+    article_id        text,
+    status int default 0 not null,
+    deleted_at  bigint,
+    created_at  bigint,
+    updated_at  bigint
+);
+
+alter table articles add column valid boolean default true not null;
+
---
+++ migrations/000003_add_authod_id_in_task.up.sql
@@ -0,0 +1,1 @@
+alter table tasks add column author_id varchar(255) not null default '';
